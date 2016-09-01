4f5c82ae36fddf3e5cac0fbcb4db90e809142a66
Nathan G. Denig <ngduser@gmail.com>
2016-09-01 18:49:49 -0500

    Fixed T Variable Error Changed FFMPEG command for video attribute information for proper assignment of T variable.

--- src/VideoUpload.php
+++ src/VideoUpload.php
@@ -1,25 +1,33 @@
 <?php
-
+	//Use AWS SDK for S3
+	require '/srv/Sites/lib/aws-autoloader.php';
+	use Aws\S3\S3Client;
+	
 	class VideoUpload {
 
 		//Static Variables
-		private $ffmpeg = '/usr/bin/ffmpeg';	
 		private $length;
 		private $bucket = 'vigilant-bucket';
-		private $s3Client = S3Client::factory(array(
-   			'profile' => 'vigilant',
-   			'region'  => 'us-west-2',
-   			'version' => 'latest'));
-		public $url;
+		private $ffmpeg;
 		
-		public function __construct($tmp_name, $name) {
+		public $s3Client;
+		public $url;
+	        public $attributes;
 
-			this->tmp_name = $tmp_name;
-			this->name = $name;
+		public function __construct($tmp_name, $name) {
+			$this->s3Client = S3Client::factory(array(
+                        'profile' => 'vigilant',
+                        'region'  => 'us-west-2',
+                        'version' => 'latest'));
+
+			$this->tmp_name = $tmp_name;
+			$this->name = $name;
 			$target_file = "uploads/" . basename($name);
 			$ext = pathinfo($target_file,PATHINFO_EXTENSION);
 			$id = null;
 			$video_name = basename($name);
+			$this->ffmpeg = "/usr/bin/ffmpeg";
+
 
 		}
 		//Sanatizes SQL Input to Prevent Injection
@@ -31,21 +39,23 @@
 		} 
 
 		function videoAttributes() {
-			$command = $ffmpeg . ' -i ' . $tmp_name .  ' -vstats 2>&1 | grep Duration';
+			$command= $this->ffmpeg . ' -i ' .  $this->tmp_name .  ' -vstats 2>&1';
 			$attributes = shell_exec($command);
-
+			$this->attributes = $attributes;
 			$regex_duration = '(Duration:\\s+)';
 			$regex_length = '((?:(?:[0-1][0-9])|(?:[2][0-3])|(?:[0-9])):(?:[0-5][0-9])(?::[0-5][0-9])?(?:\\s?)?\\.\\d+)';
 
 			if (preg_match_all ("/".$regex_duration.$regex_length."/is", $attributes, $match)) {
-      			$this->length = $match[2][0];
-      		}
+      				$this->length = $match[2][0];
+      			}
 		}
 
 		function localUpload() {
 			if (move_uploaded_file($tmp_name, $target_file)) {
         		echo "The file ". basename($name). " has been uploaded.";
-			} else {
+			} 
+			
+			else {
 				echo "Error with file upload!";
 			}
 
@@ -62,16 +72,18 @@
 			//Transfer Local Upload to Bucket
 			$filepath = "/srv/Sites/Vigilant/www/php/uploads/" . $id;
 			$operation = $s3Client->putObject(array(
-    			'Bucket'       => $bucket,
+    				'Bucket'       => $bucket,
    				'Key'          => $keyname,
-    			'SourceFile'   => $filepath,
-    			'ContentType'  => 'text/plain',
-    			'ACL'          => 'public-read',
-    			'StorageClass' => 'REDUCED_REDUNDANCY',));
-			}
+    				'SourceFile'   => $filepath,
+    				'ContentType'  => 'text/plain',
+    				'ACL'          => 'public-read',
+    				'StorageClass' => 'REDUCED_REDUNDANCY',));
 
 			$url = $operation['ObjectURL'];
+		}
 			
-			//Fix
+		function finishClean() {
 			unlink($target_file);
-?>
+		}
+	}
+?>		
