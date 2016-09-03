268f5303bc4ab9700927b79abdcd9cb320d6cee8
Nathan G. Denig <ngduser@gmail.com>
2016-09-03 13:39:05 -0500

    Additional SQL Table Added VideoData table has additional attributes and constraints. Video table depreciated.

---
+++ config/VideoData.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `VideoData` (
+	`ID` int( 11 ) NOT NULL AUTO_INCREMENT ,
+	`Duration` time DEFAULT NULL ,
+	`Bitrate` smallint( 4 ) DEFAULT NULL ,
+	`Name` varchar( 15 ) DEFAULT NULL ,
+	`Description` varchar( 255 ) DEFAULT NULL ,
+	`Location` varchar( 127 ) DEFAULT NULL ,
+	`Creation` TIMESTAMP NOT NULL DEFAULT NOW( ) ,
+	`Upload` datetime DEFAULT NULL ,
+	`File` varchar( 127 ) DEFAULT NULL ,
+	PRIMARY KEY ( `ID` )
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
