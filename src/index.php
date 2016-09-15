a37002618eabe8f502298851ef903a1d8e542daf
Nathan G. Denig <ngduser@gmail.com>
2016-09-14 21:53:18 -0500

    Edited Virtual Hosting Files for Server

---
+++ src/index.php
@@ -0,0 +1,81 @@
+<!Doctype html>
+<html>
+
+<head>
+	<link rel="stylesheet" href="bootstrap.min.css"/>
+    <link rel="stylesheet" href="sites.css"/>
+
+	   <title>Video Vortex</title>
+
+</head>
+<body>
+
+<div class="container-fluid">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="btn-group navbar-right">
+        <a href="#" class="btn btn-default btn-lg">Upload</a>
+        <a href="#" class="btn btn-default btn-lg">About</a>
+        <a href="#" class="btn btn-default btn-lg">Contact</a>
+    </div>
+    
+</nav>
+    <div class="jumbotron center-text head-theme" style="background-image: url(vortex_header.png);">
+    <h1>Video Vortex</h1>
+    <p>Content Management and Delivery System for Video Streaming</p>
+    </div>
+
+</div>
+
+<div class="container">
+
+<div class="row">
+	<div class="col-lg-3 col-sm-4 col-xs-6">
+        <a href="I am the model of a asd" class="thumbnail-attr">
+             <img src="https://s3-us-west-2.amazonaws.com/vigilant-bucket/578187f0aedfe.jpg" class="img-responsive">
+        </a>
+        <h5>illert his this ais tisa filler this isd a this is dfilelr this isd  is filelr </h5>
+    </div>
+    <div class="col-lg-3 col-sm-4 col-xs-6">
+        <a href="I am the model of a dddasd" class="thumbnail-attr">
+             <img src="https://s3-us-west-2.amazonaws.com/vigilant-bucket/578188208cc7d.jpg" class="img-responsive">
+        </a>
+        <h5>illert his this ais tisa filler this isd a this is dfilelr this isd  is filelr </h5>
+    </div>
+    <div class="col-lg-3 col-sm-4 col-xs-6">
+        <a href="I am the model of a asd" class="thumbnail-attr">
+             <img src="https://s3-us-west-2.amazonaws.com/vigilant-bucket/578188a369f7a.jpg" class="img-responsive">
+        </a>
+        </div>
+        <div class="col-lg-3 col-sm-4 col-xs-6">
+        <a href="I am the model of a asd" class="thumbnail-attr">
+             <img src="https://s3-us-west-2.amazonaws.com/vigilant-bucket/578187f0aedfe.jpg" class="img-responsive">
+        </a>
+        <h3>Rillert his this ais tisa filler this isd a this is dfilelr this isd  is filelr </h3>
+    </div>
+    
+
+     
+    
+    </div>
+ 
+</div>
+
+
+<?php
+	$starting_row = 5;
+	echo '<form action="php/index.php" method="get">
+	<button name="More" type="submit" value="$starting_row">HTML</button>;'
+
+?>
+	<script src='/home/user/node_modules/jquery/dist/jquery.min.js'> </script>
+	<script src='/home/user/node_modules/bootstrap/dist/js/bootstrap.min.js'> </script>
+</body>
+<div class="footer">
+	<div class="pull-right">
+		<h3>Nathan Garrett Denig</h3>
+	</div>
+</div>xcvdvdsf
+</html>
+
+
