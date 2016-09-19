f9b3905c5338db33828c77bf1fef5e456022810f
Nathan G. Denig <ngduser@gmail.com>
2016-09-19 15:14:11 -0500

    Changed Local Upload Moved temporary directory out of web structure to /tmp/ and linked member methods to be called in global scope of class instead of externally.

--- src/VideoUpload.php
+++ src/VideoUpload.php
@@ -33,13 +33,10 @@
 			$this->tmp_name = $tmp_name;
 			$this->name = $name;
 			$this->file = basename($name);
-//			$this->target_file = "uploads/" . basename($name);
-//			$ext = pathinfo($target_file,PATHINFO_EXTENSION);
-//			$video_name = basename($name);
 			$this->ffmpeg = "/usr/bin/ffmpeg";
-			
+		//	$this->localUpload();
 			$this->videoAttributes();
-			$this->preparedSQL();
+		//	$this->preparedSQL();
 		}
 
 		//Sanatizes SQL Input to Prevent Injection
@@ -48,48 +45,52 @@
 			$input = stripslashes($input);
 			$input = htmlspecialchars($input);
 			return $input;
-		} 
+		}
 
 		function videoAttributes() {
-			$command = $this->ffmpeg . ' -i ' .  $this->tmp_name .  ' -vstats 2>&1 | grep "Duration\|Audio\|creation" ';
+			$command = $this->ffmpeg . ' -i ' .  $this->tmp_name .  ' -vstats 2>&1 | grep "Duration\|Audio" ';
 			$attr_output = shell_exec($command);
+			echo "AAAAAAAAAAAAAA " .   $attr_output . "  AAAAAAA";
 
-			$regex = '.*?((?:2|1)\\d{3}(?:-|\\/)(?:(?:0[1-9])|(?:1[0-2]))(?:-|\\/)(?:(?:0[1-9])|(?:[1-2][0-9])|(?:3[0-1]))(?:T|\\s)(?:(?:[0-1][0-9])|(?:2[0-3])):(?:[0-5][0-9]):(?:[0-5][0-9])).*?((?:(?:[0-1][0-9])|(?:[2][0-3])|(?:[0-9])):(?:[0-5][0-9])(?::[0-5][0-9])?(?:\\s?)?).*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?(\\d\\d\\d)';
+//			$regex = '.*?((?:2|1)\\d{3}(?:-|\\/)(?:(?:0[1-9])|(?:1[0-2]))(?:-|\\/)(?:(?:0[1-9])|(?:[1-2][0-9])|(?:3[0-1]))(?:T|\\s)(?:(?:[0-1][0-9])|(?:2[0-3])):(?:[0-5][0-9]):(?:[0-5][0-9])).*?((?:(?:[0-1][0-9])|(?:[2][0-3])|(?:[0-9])):(?:[0-5][0-9])(?::[0-5][0-9])?(?:\\s?)?).*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?(\\d\\d\\d)';
+
+			$regex = '.*?((?:2|1)\\d{3}(?:-|\\/)(?:(?:0[1-9])|(?:1[0-2]))(?:-|\\/)(?:(?:0[1-9])|(?:[1-2][0-9])|(?:3[0-1]))(?:T|\\s)(?:(?:[0-1][0-9])|(?:2[0-3])):(?:[0-5][0-9]):(?:[0-5][0-9])).*?((?:(?:[0-1][0-9])|(?:[2][0-3])|(?:[0-9])):(?:[0-5][0-9])(?::[0-5][0-9])?(?:\\s?)?).*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?\\d.*?(\\d\\d\\d)';			
 
 			if (preg_match_all ("/".$regex."/is", $attr_output, $match)) {
- 				$this->created = $this->verifyInput($match[1][0]);
-   				$this->duration = $this->verifyInput($match[2][0]);
-     				$this->bitrate = $this->verifyInput($match[3][0]);
+			 echo "AAAAAAAAAAAAAA " .   $attr_output . "  AAAAAAA";
+
+				$this->created = $match[1][0];
+  				$this->duration = $match[2][0];
+     				$this->bitrate = $match[3][0];
 			}
-			
 			$this->description = $this->verifyInput($_POST['description']);
 			$this->uuid = uniqid();
 		}
 
 		function localUpload() {
-			if (move_uploaded_file($this->tmp_name, "uploads/" . $this->file )) {
+			if (move_uploaded_file($this->tmp_name,  "/tmp/" . $this->file )) {
 
+				$this->makeThumbnail();
+				$this->uploadFile();
 			}
 			else {
 				echo "Error with file upload!";
 			}
-
 		}
 
-		function makeThumbnail() {	
-			$thumb = "uploads/" . $this->uuid . ".jpg";
-			$cmd = $this->ffmpeg . ' -i ' . "uploads/" . $this->name . ' -deinterlace -an -ss 10 -f mjpeg -t 1 -r 1 -y -s 240x135 ' . $thumb . ' 2>&1';
+		function makeThumbnail() {
+			$thumb = "/tmp/" . $this->uuid . ".jpg";
+			$cmd = $this->ffmpeg . ' -i ' . "/tmp/" . $this->name . ' -deinterlace -an -ss 10 -f mjpeg -t 1 -r 1 -y -s 240x135 ' . $thumb . ' 2>&1';
 			$cmd_output = exec($cmd);
 		}
 
 		function uploadFile() {
-			
-			$keyname = $this->uuid . "." . pathinfo("uploads/" . $this->file,PATHINFO_EXTENSION);
+
+			$keyname = $this->uuid . "." . pathinfo("/tmp/" . $this->file,PATHINFO_EXTENSION);
 
 
 			//Transfer Local Upload to Bucket
-			$filepath = "/srv/Sites/Vigilant/www/php/uploads/" . $this->file;
-			
+			$filepath = "/tmp/" . $this->file;
          		$video_operation = $this->s3Client->putObject(array(
                                 'Bucket'       => $this->bucket,
                                 'Key'          => $keyname,
@@ -97,10 +98,9 @@
                                 'ContentType'  => 'text/plain',
                                 'ACL'          => 'public-read',
                                 'StorageClass' => 'REDUCED_REDUNDANCY',));
-			
 			$thmb_name = $this->uuid . ".jpg";
-			$path = "/srv/Sites/Vigilant/www/php/uploads/" . $thmb_name;
-		
+
+			$path = "/tmp/" . $thmb_name;
 			$thumb_operation = $this->s3Client->putObject(array(
     				'Bucket'       => $this->bucket,
    				'Key'          => $thmb_name,
@@ -108,6 +108,8 @@
     				'ContentType'  => 'text/plain',
     				'ACL'          => 'public-read',
     				'StorageClass' => 'REDUCED_REDUNDANCY',));
+			unlink("/tmp/" . $this->name);
+			unlink($path);
 		}
 
 		function preparedSQL() {
@@ -118,24 +120,19 @@
 			else{
 				$connection->query("INSERT INTO VideoData (UUID, Duration, Name, Description, Bitrate, Created)
 					VALUES ('$this->uuid', '$this->duration', '$this->name', '$this->description', '$this->bitrate', '$this->created')");
-
 			}
 
 			mysqli_close($connection);
 		}
-		
-		function finishClean() {
-			unlink($target_file);
-		}
-		
+
 		function showAll() {
-			echo $this->uuid;
-			echo $this->duration;
-			echo $this->name;
-			echo $this->description;
-			echo $this->bitrate;
+			echo "UUID - " . $this->uuid;
+			echo "DURATION - " . $this->duration;
+			echo "NAME " . $this->name;
+			echo "DESCRIPTION - " .  $this->description;
+			echo "BITRATE = " . $this->bitrate;
 			//echo $this->uploaded;
-			echo $this->created;
+			echo "CREATED   =  " . $this->created;
 			//echo $this->file;
 		}
 	}
