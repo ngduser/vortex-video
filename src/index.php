92f1bcd42bf0e08bf3b48a816196ac67c6892294
Nathan G. Denig <ngduser@gmail.com>
2016-09-19 15:12:05 -0500

    Made Dynamic Index Page to Generate Site Contents From Prepared SQL Statement Index page is now dynamic and generates rows of SD3 stored thumbnails based on database query.

--- src/index.php
+++ src/index.php
@@ -2,8 +2,8 @@
 <html>
 
 <head>
-	<link rel="stylesheet" href="bootstrap.min.css"/>
-    <link rel="stylesheet" href="sites.css"/>
+    <link rel="stylesheet" href="/lib/bootstrap.min.css"/>
+    <link rel="stylesheet" href="/lib/sites.css"/>
 
 	   <title>Video Vortex</title>
 
@@ -12,70 +12,85 @@
 
 <div class="container-fluid">
 
-<nav class="navbar navbar-default navbar-fixed-top">
-	<div class="btn-group navbar-right">
-        <a href="#" class="btn btn-default btn-lg">Upload</a>
-        <a href="#" class="btn btn-default btn-lg">About</a>
-        <a href="#" class="btn btn-default btn-lg">Contact</a>
-    </div>
-    
-</nav>
-    <div class="jumbotron center-text head-theme" style="background-image: url(vortex_header.png);">
-    <h1>Video Vortex</h1>
-    <p>Content Management and Delivery System for Video Streaming</p>
-    </div>
-
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="btn-group navbar-right">
+        	<a href="#" class="btn btn-default btn-lg">Upload</a>
+        	<a href="#" class="btn btn-default btn-lg">About</a>
+        	<a href="#" class="btn btn-default btn-lg">Contact</a>
+	</nav>
+<div>
+<div class="jumbotron center-text head-theme" style="background-image: url(/lib/vortex_header.png);">
+    	<h1>Video Vortex</h1>
+    	<p>Video Streaming Content Management and Delivery</p>
 </div>
-
-<div class="container">
-
-<div class="row">
-	<div class="col-lg-3 col-sm-4 col-xs-6">
-        <a href="I am the model of a asd" class="thumbnail-attr">
-             <img src="https://s3-us-west-2.amazonaws.com/vigilant-bucket/578187f0aedfe.jpg" class="img-responsive">
-        </a>
-        <h5>illert his this ais tisa filler this isd a this is dfilelr this isd  is filelr </h5>
-    </div>
-    <div class="col-lg-3 col-sm-4 col-xs-6">
-        <a href="I am the model of a dddasd" class="thumbnail-attr">
-             <img src="https://s3-us-west-2.amazonaws.com/vigilant-bucket/578188208cc7d.jpg" class="img-responsive">
-        </a>
-        <h5>illert his this ais tisa filler this isd a this is dfilelr this isd  is filelr </h5>
-    </div>
-    <div class="col-lg-3 col-sm-4 col-xs-6">
-        <a href="I am the model of a asd" class="thumbnail-attr">
-             <img src="https://s3-us-west-2.amazonaws.com/vigilant-bucket/578188a369f7a.jpg" class="img-responsive">
-        </a>
-        </div>
-        <div class="col-lg-3 col-sm-4 col-xs-6">
-        <a href="I am the model of a asd" class="thumbnail-attr">
-             <img src="https://s3-us-west-2.amazonaws.com/vigilant-bucket/578187f0aedfe.jpg" class="img-responsive">
-        </a>
-        <h3>Rillert his this ais tisa filler this isd a this is dfilelr this isd  is filelr </h3>
-    </div>
-    
-
-     
-    
-    </div>
- 
-</div>
-
 
 <?php
-	$starting_row = 5;
-	echo '<form action="php/index.php" method="get">
-	<button name="More" type="submit" value="$starting_row">HTML</button>;'
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
 
+	if (isset($_GET['More'])) {
+	    $starting_row = $_GET['More'];
+	}
+
+	else {
+	    $starting_row = 0;
+	}
+
+	$connection = new mysqli("localhost", "vortex", "testpassword", "vigilant");
+
+	if ($connection->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+
+	else {
+		$result =  $connection->query("SELECT vid_filepath, tn_id, name FROM Video LIMIT $starting_row,20");
+
+		$vid_array = array();
+
+		while ($row = mysqli_fetch_array($result)) {
+			$vid_array[] = $row;
+		}
+
+		$builder_start = '<div class="container"> <div class="row">';
+		$builder_end = '</div>';
+		$builder_row = '<div class="row">';
+		$builder_1 = '<div class="col-lg-3 col-sm-4 col-xs-6"> <a href="';
+		$builder_2 = '" class="thumbnail-attr"> <img src="';
+		$builder_3 = '" class="img-responsive"> </a> <h3>';
+		$builder_4 = '</h3> </div>';
+
+		$builder_newline = "\n";
+
+		$i = 0;
+		$tn_id = "tn_id";
+		$name = "name";
+		$vid_filepath = "vid_filepath";
+
+		echo $builder_start;
+		foreach ($vid_array as $key => $value) {
+			echo $builder_newline, $builder_1, $value[$vid_filepath], $builder_2, $value[$tn_id], $builder_3, $value[$name], $builder_4, $builder_newline;
+		}
+
+		echo $builder_end, $builder_newline;
+
+		if (count($vid_array < 20)) {
+			$starting_row = 0;
+		}
+
+		else {
+			$starting_row = $starting_row + 5;
+		}
+
+	}
+	
+	echo "<form action='index.php' method='get'>
+	<button name='More' type='submit' value='$starting_row' style='border: none; background: none; padding: 0;'><h2>More</h2></button>";
+	mysqli_close($connection);
 ?>
-	<script src='/home/user/node_modules/jquery/dist/jquery.min.js'> </script>
-	<script src='/home/user/node_modules/bootstrap/dist/js/bootstrap.min.js'> </script>
+	<script src='/lib/jquery.min.js'> </script>
+	<script src='/lib/bootstrap.min.js'> </script>
 </body>
-<div class="footer">
-	<div class="pull-right">
-		<h3>Nathan Garrett Denig</h3>
-	</div>
-</div>xcvdvdsf
 </html>
+
 
 
