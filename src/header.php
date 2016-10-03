80c3f30947d6447dff110ccc6cca00187e5b243f
Nathan G. Denig <ngduser@gmail.com>
2016-10-03 13:45:03 -0500

    Added HTML Templates

---
+++ src/header.php
@@ -0,0 +1,29 @@
+<!Doctype html>
+<html>
+<head>
+    <title>Video Vortex</title>
+
+    <link rel="stylesheet" href="/lib/bootstrap.min.css"/>
+    <link rel="stylesheet" href="/lib/sites.css"/>
+
+    <link href="http://www.videovortex.stream/webapp/video.js/dist/video-js.css" rel="stylesheet">
+    <script src="http://www.videovortex.stream/webapp/video.js/dist/video.js"></script>
+
+    <script>
+    	videojs.options.flash.swf= "http://www.videovortex.stream/webapp/video.js/dist/video-js.swf"
+    </script>
+</head>
+<body>
+<div class="container-fluid">
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="btn-group navbar-right">
+        	<a href="/upload.html" class="btn btn-default btn-lg">Upload</a>
+        	<a href="#" class="btn btn-default btn-lg">About</a>
+        	<a href="#" class="btn btn-default btn-lg">Contact</a>
+		</div>
+	</nav>
+</div>
+<div class="jumbotron center-text head-theme" style="background-image: url(/lib/d_cloud.gif);">
+    	<h1>Video Vortex</h1>
+    	<p>Video Streaming Content Management and Delivery</p>
+</div>
